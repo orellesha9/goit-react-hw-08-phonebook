fa5493393a2d02b2840fe8e8d45ddac657a204b9
orellesha9 <63074265+orellesha9@users.noreply.github.com>
2024-02-21 20:17:01 +0000

    Deploying to gh-pages from @ orellesha9/goit-react-hw-08-phonebook@893bd49c2fc94ba36c660561ddbe8b8b37d0177f 🚀

---
+++ static/js/540.5d8cc3e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[540],{540:function(e,t,n){n.r(t);var r=n(1087),a=n(184);t.default=function(){return(0,a.jsxs)("div",{children:[(0,a.jsx)("h1",{children:"Cannot found this page"}),(0,a.jsx)(r.rU,{to:"/",children:"To home page"})]})}}}]);
+//# sourceMappingURL=540.5d8cc3e0.chunk.js.map
