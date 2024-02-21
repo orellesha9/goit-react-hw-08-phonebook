2f8421ea64c6e42200b9cd6929e0f9b89eee5611
orellesha9 <orellesha9@gmail.com>
2024-02-21 22:22:53 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+
 import { useDispatch } from 'react-redux';
 import { current } from './redux/auth/auth-operations';
 
@@ -16,9 +16,9 @@ export const App = () => {
     <div>
       {/* //   React homework template */}
       {/* <Navbar /> */}
-      <BrowserRouter basename='goit-react-hw-08-phonebook'>
+     
       <AppRoutes />
-      </BrowserRouter>
+    
       {/* <AllMyNumbers/> */}
     </div>
   );
